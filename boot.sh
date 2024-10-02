84155748fc6f3d406d378a661acec16c44899a5d
Wheel Smith <kristjan.rattasepp@gmail.com>
2024-10-02 18:24:43 +0300

    boot.sh updated to Wheel_Smith

--- boot.sh
+++ boot.sh
@@ -1,16 +1,21 @@
 set -e
 
-ascii_art='________                  __        ___.
-\_____  \   _____ _____  |  | ____ _\_ |__
- /   |   \ /     \\__   \ |  |/ /  |  \ __ \
-/    |    \  Y Y  \/ __ \|    <|  |  / \_\ \
-\_______  /__|_|  (____  /__|_ \____/|___  /
-        \/      \/     \/     \/         \/
-'
+ascii_art=$(
+  cat <<'EOF'
+ __    __  _                  _         __             _  _    _     
+/ / /\ \ \| |__    ___   ___ | |       / _\ _ __ ___  (_)| |_ | |__  
+\ \/  \/ /| '_ \  / _ \ / _ \| |       \ \ | '_ ` _ \ | || __|| '_ \ 
+ \  /\  / | | | ||  __/|  __/| |       _\ \| | | | | || || |_ | | | |
+  \/  \/  |_| |_| \___| \___||_| _____ \__/|_| |_| |_||_| \__||_| |_|
+                                |_____|                                 
+EOF
+)
 
-echo -e "$ascii_art"
+echo "$ascii_art"
 echo "=> Omakub is for fresh Ubuntu 24.04 installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
+
+exit 1
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
@@ -19,9 +24,9 @@ echo "Cloning Omakub..."
 rm -rf ~/.local/share/omakub
 git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub
-	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
-	cd -
+  cd ~/.local/share/omakub
+  git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
+  cd -
 fi
 
 echo "Installation starting..."
